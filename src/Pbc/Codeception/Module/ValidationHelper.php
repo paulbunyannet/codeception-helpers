db309fb1bc7ce5db0f02f932fc99d12277f4cd53
Nate Nolting <me@natenolting.com>
2019-05-09 14:10:49 -0500

    Update ValidationHelper.php
    Made the output message a but more verbose.

--- src/Pbc/Codeception/Module/ValidationHelper.php
+++ src/Pbc/Codeception/Module/ValidationHelper.php
@@ -34,11 +34,35 @@ class ValidationHelper extends CodeceptionModule
         $result = json_decode(file_get_contents($outputFilePath));
         codecept_debug($result);
         if (property_exists($result, 'messages') && count($result->messages) > 0) {
-            $I->fail((new Colors())->getColoredString(
-                $I->grabFromCurrentUrl() . PHP_EOL .
-                ucfirst($result->messages[0]->type) . ' on line ' . $filePath . PHP_EOL .
-                (string)$result->messages[0]->lastLine . ': ' . $result->messages[0]->message . '(' . $result->messages[0]->extract . ')', 'red')
-            );
+
+            for($i=0, $iCount=count($result->messages); $i < $iCount; $i++) {
+                if ($result->messages[$i]->type === 'error') {
+                    $this->renderResponse($I, $result->messages[$i], 'fail', 'red', $filePath);
+                }
+                $this->renderResponse($I, $result->messages[$i], 'comment', 'yellow', $filePath);
+            }
         }
+    }
+
+    /**
+     * @param \AcceptanceTester $I
+     * @param object            $response The response array from W3C
+     * @param string            $type     Type of output, `comment` or `fail`
+     * @param string            $color    Type color
+     * @param string            $filePath Path to the checked file
+     */
+    protected function renderResponse($I, $response, $type, $color, $filePath)
+    {
+        $I->{$type}(
+            (new Colors())
+                ->getColoredString(
+                    'On URL: ' . $I->grabFromCurrentUrl() . PHP_EOL .
+                    ucfirst($response->type) .
+                    (property_exists($response,'subType') ? ' ('.  ucfirst($response->subType) .')' : '') .
+                    ' on line ' . $filePath . ':' . (string)$response->lastLine . PHP_EOL .
+                    (string)$response->lastLine . ': ' . $response->message . '(' . $response->extract . ')',
+                    $color
+                )
+        );
     }
 }
